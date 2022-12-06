9f88431cdcec2a8354d5f26af0a32c6ce1a19d0f
GumarGarifullyn <118556134+GumarGarifullyn@users.noreply.github.com>
2022-12-06 18:59:08 +0300

    add task2_4

---
+++ task2_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход
+// число и проверяет, кратно ли оно одновременно
+// 7 и 23.
+// ● 14 -> нет
+// ● 46 -> нет
+// ● 161 -> да 
+
+Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine()!);
+if (A % 7 == 0 && A % 23 == 0) Console.WriteLine("Yes");
+else Console.WriteLine("No");
