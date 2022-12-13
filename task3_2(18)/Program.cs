725eb2fafea8b73498b0d6876e70a3ae247ea87a
GumarGarifullyn <118556134+GumarGarifullyn@users.noreply.github.com>
2022-12-13 16:33:10 +0300

    add task 18

---
+++ task3_2(18)/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача №18. Работа в группах
+// Напишите программу, которая по заданному номеру
+// четверти, показывает диапазон возможных координат
+// точек в этой четверти (x и y).
+
+
+Console.WriteLine("Введите номер четверти от 1 до 4:");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num == 1) Console.WriteLine("x > 0; y > 0");
+if (num == 2) Console.WriteLine("x < 0; y > 0");
+if (num == 3) Console.WriteLine("x < 0; y < 0");
+if (num == 4) Console.WriteLine("x > 0; y < 0");
+if(num <= 0 && num > 4) Console.WriteLine("Введен неверный диапазон");
