05ad119949597e56029fa405801556de53444a6f
GumarGarifullyn <118556134+GumarGarifullyn@users.noreply.github.com>
2022-12-13 21:31:01 +0300

    add task 21

--- task3_2(18)/Program.cs
+++ task3_2(18)/Program.cs
@@ -11,4 +11,4 @@ if (num == 1) Console.WriteLine("x > 0; y > 0");
 if (num == 2) Console.WriteLine("x < 0; y > 0");
 if (num == 3) Console.WriteLine("x < 0; y < 0");
 if (num == 4) Console.WriteLine("x > 0; y < 0");
-if(num <= 0 && num > 4) Console.WriteLine("Введен неверный диапазон");
+if(num <= 0 && num > 4) Console.WriteLine("Введен неверный диапазон ");
---
+++ task3_3(21)/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача №21. Работа в группах
+// Напишите программу, которая принимает на вход
+// координаты двух точек и находит расстояние между
+// ними в 2D пространстве.
+// ● A (3,6); B (2,1) -> 5,09
+// ● A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine("Введите координату x точка A:");
+int ax = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату y точки A:");
+int ay = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату x точка B:");
+int bx = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату y точки B:");
+int by = int.Parse(Console.ReadLine()!);
+double dis = Math.Round(Math.Sqrt((bx-ax)*(bx-ax)+(by-ay)*(by-ay)),2);
+Console.WriteLine(dis);
